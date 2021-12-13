483fa4776089254057ef24f9bc1dbbf53f45e32a
Aurelie Berniche <berniche.aurelie@orange.fr>
2021-12-13 15:02:06 +0100

    missing contributor permission

--- registration/permissions.py
+++ registration/permissions.py
@@ -1,14 +1,23 @@
 from rest_framework.permissions import IsAuthenticated, BasePermission
+from registration.models import Contributors
 
 
 class IsAuhtorFromTheProject(BasePermission):
  
     def has_object_permission(self, request, view, project):
+        print("you're the author")
         return project.author_user_id == request.user
         #  return bool(request.user==request.data["author_user_id"])
 
 
 class IsContributorFromTheProject(BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        pass
+    def has_object_permission(self, request, view, project):
+        contributor = Contributors.objects.get(project_id = project.id)
+        print("permission?")
+        if request.user == contributor.user_id:
+            print("Allowed: You're contributor")
+            return True
+        else:
+            print("Acces denied: You're not contributor for the project")
+            return False
